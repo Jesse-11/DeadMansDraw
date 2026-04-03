98cdf122e67d7d0a11948c128ed71ec5983882c3
Jesse Hoppo <jesse.hoppo11@gmail.com>
2026-04-03 16:21:55 +1030

    Added cardtypes and basic coding
    - Added anchor for bonus marks
    - Forward decalred card so i can typedef the cardcollection based on the assignment spec

--- DeadMansDraw/Types.h
+++ DeadMansDraw/Types.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <vector>
+
+enum class CardType {
+	Cannon, Chest, Key, Anchor, Kraken, Map, Oracle, Mermaid, Hook, Sword
+};
+
+class Card;
+
+//Recommend in assignment spec.
+typedef std::vector<Card*> CardCollection;
+
+#endif // TYPES_H
