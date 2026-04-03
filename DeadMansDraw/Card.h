91499ed9c7958b91a0524581a821e11f9c96f31d
Jesse Hoppo <jesse.hoppo11@gmail.com>
2026-04-03 16:35:46 +1030

    Added basic dec/def for card
    - Implemented all functionality as needed, mainly based off assignment spec
    - Added an aditional function that is virtual, used to check for chest and key functionality

--- DeadMansDraw/Card.h
+++ DeadMansDraw/Card.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+#include "Types.h"
+
+class Game;
+class Player;
+
+class Card {
+protected:
+    CardType type;
+    int value;
+
+public:
+    Card(CardType cardType, int cardValue) : type(cardType), value(cardValue) {}
+    virtual ~Card() {}
+
+    const CardType& getCardType() const { 
+        return type; 
+    }
+
+    int getCardValue() const { 
+        return value; 
+    }
+
+    virtual std::string str() const = 0; 
+
+    virtual void play(Game& game, Player& player) = 0;
+
+    virtual void chestAndKey(Game& game, Player& player) {}
+
+};
+
+
+
+#endif //CARD_H
